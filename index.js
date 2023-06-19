ea11aa31e0f0232f7563d45060d267a038082293
Nuria <c.odena.nuria@gmail.com>
2023-06-19 20:02:03 +0200

    first commit

--- index.js
+++ index.js
@@ -1,165 +1,53 @@
-// // Displaying messages
-// console.log("hello world");
-
-// //Debugging
-// const x = 5;
-// console.log('The value of x is:', x);
-
-// //Interact with js
-// console.log(2 + 2);
-
-// //Different console methods
-// console.warn('this is a warning');
-// console.error('this is an error');
-// console.info('this is an info message');
+//Function declaration
+function functionName(){
+ return 'abc'
+}
+//Function invokation
+console.log(functionName())
 
 
-// const quantity = null;
-// let moreQuantity;
+//Function declaration with parameters
+function calculateArea(length, width){
+  return length * width
+}
+ //Function invokation with parameters
+console.log(calculateArea(5, 5))
 
-// console.log(quantity); // Output null
-// console.log(moreQuantity); //Output undefined
+ //Function declaration with default parameters
+function calculateArea(length = 6, width = 10){
+  return length * width
+}
+ //Function invokation with default parameters
+ console.log(calculateArea(7, 7))
 
-// //Let vs var
-
-// var pikachu = "pikachu";
-// let jigglypuff = "jigglypuff";
-
-// console.log(pikachu); // pikachu
-// console.log(jigglypuff); // jigglypuff
-
-// if (true) {
-//  var pikachu = "I don't know who I am anymore :(";
-//  let jigglypuff = "I don't know who I am anymore :(";
-
-//  console.log(jigglypuff); 
-// }
-
-// console.log(pikachu); // I don't know who I am anymore :(
-// console.log(jigglypuff); // jigglypuff
-
-// //GLOBAL VS FUNCTION SCOPE
-
-// let someVariable;
-
-// if(true){
-//     someVariable = 'else';
-// } 
-
-// console.log(someVariable)
-
-//If/else statements
-
-//const number = 5;
-
-// if (number === 2) {
-//     console.log('the value is 2')
-// } else if (number === 3) {
-//     console.log('the value is 3')
-// } else if (number === 5) {
-//     console.log('the value is 5')
-// } else {
-//     console.log('I do not know this value :(')
-// }
-
-//Ternaries
-// const number = 5;
-// const message = number === 5 ? 'the number is 5' : 'I do not know this number';
-// console.log(message) // the number is 5
-
-// Switch statements
-
-// const pokemon = 'Pikachu';
-
-// switch (pokemon) {
-//  case 'Pikachu':
-//    console.log('We restored your Pikachu to full health!');
-//    break;
-//  case 'Squirtle':
-//  case 'Magikarp':
-//    console.log('We restored your water pokemon to full health!');
-//    break;
-//  default:
-//    console.log('Sorry, we cannot heal your pokemon :(');
-// }
-
-//Arrays
-
-//const pokemon = ['Pikachu', 'Squirtle', 'Charmander']
-// console.log(pokemon[0]) // Pikachu
-
-// const reversePokemon = pokemon.reverse();
-// console.log(reversePokemon) // ['Charmander', 'Squirtle', 'Pikachu']
-
-//Functions
-
-// function talk () {
-//     console.log('I have no mouth, and I must scream');
-//     console.log('I have no mouth, and I must scream');
-//     console.log('I have no mouth, and I must scream');
-// }
-
-// talk();
-   
-// function talkDynamic (message, pokemon) {
-//     console.log('Hello' + '' + message + ' ' + pokemon);
-// }
-
-// talkDynamic('greetings', 'pikachu');
-// talkDynamic('welcome', 'to class');
-   
-
-//Loops
-
-//FOR
-
-// for (let i = 2; i < 10; i++) {
-//     console.log(i, 'foor lop');
-// }
-
-//const pokemon = ['Pikachu', 'Squirtle', 'Charmander', 'Charmander'];
+ console.log(helloFunctionDeclaration('Núria')) 
+ function helloFunctionDeclaration(name = 'John Doe') {
+  return `Hello ${name}`;
+ }
+ 
+ //Anonymous FUNCTIONS
+ setTimeout(function() {
+  //console.log('This function is gonna be executed after a delay')
+ }, 4000)
 
 
-// console.log(pokemon[0])
-// console.log(pokemon[1])
-// console.log(pokemon[2])
+ //Arrow functions
+ const helloArrow = (name) => {
+  return `Hello ${name}`;
+ }
 
-//Increase per 1 
+ console.log(helloArrow('Núria'))
 
-// for (let i = 0; i < pokemon.length; i++) {
-//     console.log(`I encountered ${pokemon[i]} pokemon so far`);
-// }
+ const helloArrowAbstracted = (name) => `Hello ${name}`;
+ 
+//Recursive functions
 
-//Increase per 2 
-
-// for (let i = 0; i < pokemon.length; i += 2) {
-//     console.log(`I encountered ${pokemon[i]} pokemon so far`);
-// }
-  
-
-//WHILE
-//Checks condition before iteration
-
-// let i = 0;
-// while (i < 5) {
-//   console.log(i);
-//   i++;
-// }
-
-//DO WHILE
-//Checks condition after iteration
-
-// let i = 0;
-// do {
-//   console.log(i);
-//   i++;
-// } while (i < 5);
-
-//FOR EACH == FOR LOOP
-
-// const numbers = [1, 2, 3, 4, 5, 8, 10, 11];
-
-// numbers.forEach((whatever, index) => {
-// console.log(`Element at index ${index} is ${whatever}`);
-// console.log('Element at index ' + index +  ' is ' + whatever);
-// });
+const factorial = function (number) {
+  if (number <= 0) {
+    return 1;
+  } else {
+    return (number * factorial(number - 1));
+  }
+ };
+ 
+ console.log(factorial(6));
