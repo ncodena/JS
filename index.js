69003a059cc44d9f0260a11aa097f4cbdc4fc1af
Nuria <c.odena.nuria@gmail.com>
2023-07-06 21:47:49 +0200

    adding code

--- index.js
+++ index.js
@@ -1,40 +1,113 @@
-let form = document.querySelector('form');
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-  if (document.querySelector('input').value.trim() === "") {
-    alert("Please Enter a Task");
-  } else {
-    let taskName = document.querySelector('input').value;
-    let taskElement = `
-        <div class="task">
-        <span class="taskname">
-            ${taskName}
-        </span>
-        <input type="checkbox" class="check">
-        <button class="edit">
-            Edit
-        </button>
-        <button class="delete">
-            Delete
-        </button>
-        </div>
-    `;
-    document.querySelector('#tasks').innerHTML += taskElement;
-  }
-});
-
-let tasksContainer = document.querySelector("#tasks");
-tasksContainer.addEventListener("click", (event) => {
-  if (event.target.classList.contains("delete")) {
-    event.target.parentNode.remove();
-  } else if (event.target.classList.contains("edit")) {
-    let taskNameElement = event.target.parentNode.querySelector(".taskname");
-    let newTaskName = prompt("Enter the new task name:", taskNameElement.textContent.trim());
-    if (newTaskName !== null) {
-      taskNameElement.textContent = newTaskName;
+class Book {
+    #title;
+    #genre;
+    #author;
+    #read;
+    #readDate;
+    constructor(title, genre, author){
+        this.#title = title;
+        this.#genre = genre;
+        this.#author = author;
+        this.#read = false;
+        this.#readDate = null;
     }
-  } else if (event.target.classList.contains("check")) {
-    let taskNameElement = event.target.parentNode.querySelector(".taskname");
-    taskNameElement.classList.toggle("completed");
-  }
-});
+
+    get read(){
+        return this.#read;
+    }
+
+    get readDate(){
+        return this.#readDate;
+    }
+
+    markAsRead() {
+        this.#read = true;
+        this.#readDate = new Date(Date.now());
+    }
+}
+
+class BookList {
+    #books
+    #readCount
+    #unreadCount
+    #nextBook
+    #currentBook
+    #lastBook
+    constructor(){
+        this.#books = [];
+        this.#readCount = 0;
+        this.#unreadCount = 0;
+        this.#currentBook = null;
+        this.#nextBook = null;
+        this.#lastBook = null;
+    }
+
+    //Add a book to the book list
+
+    addBook(book){
+        this.#books.push(book);
+        this.#nextBook = book;
+        //Increment count of books unread when adding new book
+        this.#unreadCount++;
+    }
+
+    finishCurrentBook(){
+        if(this.#currentBook === null){
+            console.log('no book is currently read')
+        }
+        //Mark the book as read
+        this.#currentBook.markAsRead();
+        //Change the last book read to be the book that just got finished
+        this.#lastBook = this.#currentBook;
+        //Change the current book to be the next book to be read
+        this.#currentBook = this.#nextBook
+        //Find the first unread book and change the next book to be read propery to be the first unread book you find in the list of books
+        for(let book of this.#books){
+            if(!book.read){
+                this.#nextBook = book;
+                break;
+            }
+        }
+
+        //Increment count of books read because we finished reading a new one and we can count it as read
+        this.#readCount++;
+
+        //Decrement count of books unread because we finished reading a new one
+        this.#unreadCount--;
+    }
+
+    get books(){
+        return this.#books;
+    }
+    get unreadCount(){
+        return this.#unreadCount;
+    }
+    get readCount(){
+        return this.#readCount;
+    }
+    get currentBook(){
+        return this.#currentBook;
+    }
+
+    set currentBook(book){
+        this.#currentBook = book;
+    }
+
+    get nextBook(){
+        return this.#nextBook;
+    }
+
+    set nextBook(book){
+        this.#nextBook = book;
+    }
+}
+
+const myBookList = new BookList();
+const book1 = new Book("Book 1", "Fiction", "Author 1")
+const book2 = new Book("Book 2", "Fiction 2", "Author 2")
+
+myBookList.addBook(book1);
+myBookList.addBook(book2);
+myBookList.currentBook = myBookList.nextBook;
+myBookList.finishCurrentBook();
+console.log(myBookList)
