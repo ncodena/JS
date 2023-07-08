27a5f1553fcd874ed3014b8ffa13b2a5df3e478f
Nuria <c.odena.nuria@gmail.com>
2023-07-08 12:49:22 +0200

    add code

--- index.js
+++ index.js
@@ -1,40 +1,74 @@
-let form = document.querySelector('form');
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-  if (document.querySelector('input').value.trim() === "") {
-    alert("Please Enter a Task");
-  } else {
-    let taskName = document.querySelector('input').value;
-    let taskElement = `
-        <div class="task">
-        <span class="taskname">
-            ${taskName}
-        </span>
-        <input type="checkbox" class="check">
-        <button class="edit">
-            Edit
-        </button>
-        <button class="delete">
-            Delete
-        </button>
-        </div>
-    `;
-    document.querySelector('#tasks').innerHTML += taskElement;
-  }
-});
-
-let tasksContainer = document.querySelector("#tasks");
-tasksContainer.addEventListener("click", (event) => {
-  if (event.target.classList.contains("delete")) {
-    event.target.parentNode.remove();
-  } else if (event.target.classList.contains("edit")) {
-    let taskNameElement = event.target.parentNode.querySelector(".taskname");
-    let newTaskName = prompt("Enter the new task name:", taskNameElement.textContent.trim());
-    if (newTaskName !== null) {
-      taskNameElement.textContent = newTaskName;
+class TaskItem {
+    constructor(name) {
+      this.name = name;
+      this.completed = false;
     }
-  } else if (event.target.classList.contains("check")) {
-    let taskNameElement = event.target.parentNode.querySelector(".taskname");
-    taskNameElement.classList.toggle("completed");
+  
+    toggleCompletion() {
+      this.completed = !this.completed;
+    }
+  
+    updateName(newName) {
+      this.name = newName;
+    }
   }
-});
+  
+  class TaskManager {
+    constructor() {
+      this.form = document.querySelector('form');
+      this.tasksContainer = document.querySelector('#tasks');
+      this.form.addEventListener('submit', this.addTask.bind(this));
+      this.tasksContainer.addEventListener('click', this.handleTaskActions.bind(this));
+    }
+  
+    addTask(event) {
+      event.preventDefault();
+      const input = document.querySelector('input');
+      const taskName = input.value.trim();
+      if (taskName === "") {
+        alert("Please Enter a Task");
+      } else {
+        const task = new TaskItem(taskName);
+        this.renderTask(task);
+        input.value = "";
+      }
+    }
+  
+    renderTask(task) {
+      const taskElement = document.createElement('div');
+      taskElement.classList.add('task');
+      taskElement.innerHTML = `
+        <span class="taskname">${task.name}</span>
+        <input type="checkbox" class="check">
+        <button class="edit">Edit</button>
+        <button class="delete">Delete</button>
+      `;
+      this.tasksContainer.appendChild(taskElement);
+    }
+  
+    handleTaskActions(event) {
+      const target = event.target;
+      if (target.classList.contains("delete")) {
+        target.parentNode.remove();
+      } else if (target.classList.contains("edit")) {
+        this.editTask(target);
+      } else if (target.classList.contains("check")) {
+        this.toggleTaskCompletion(target);
+      }
+    }
+  
+    editTask(target) {
+      const taskNameElement = target.parentNode.querySelector(".taskname");
+      const newTaskName = prompt("Enter the new task name:", taskNameElement.textContent.trim());
+      if (newTaskName !== null) {
+        taskNameElement.textContent = newTaskName;
+      }
+    }
+  
+    toggleTaskCompletion(target) {
+      const taskNameElement = target.parentNode.querySelector(".taskname");
+      taskNameElement.classList.toggle("completed");
+    }
+  }
+  
+  const taskManager = new TaskManager();
