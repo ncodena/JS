c44cdf395a4736337d118e0c3494bcd59ed12138
Nuria <c.odena.nuria@gmail.com>
2023-07-08 16:07:24 +0200

    adding code

--- index.js
+++ index.js
@@ -1,40 +1,72 @@
-let form = document.querySelector('form');
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-  if (document.querySelector('input').value.trim() === "") {
-    alert("Please Enter a Task");
-  } else {
-    let taskName = document.querySelector('input').value;
-    let taskElement = `
-        <div class="task">
-        <span class="taskname">
-            ${taskName}
-        </span>
-        <input type="checkbox" class="check">
-        <button class="edit">
-            Edit
-        </button>
-        <button class="delete">
-            Delete
-        </button>
-        </div>
-    `;
-    document.querySelector('#tasks').innerHTML += taskElement;
-  }
-});
+//const input = document.querySelector('input');
+const form = document.querySelector("form");
+const list = document.querySelector("#listItems");
+const itemsInput = form.elements['items'];
+const inputText = document.querySelector('#txt');
 
-let tasksContainer = document.querySelector("#tasks");
-tasksContainer.addEventListener("click", (event) => {
-  if (event.target.classList.contains("delete")) {
-    event.target.parentNode.remove();
-  } else if (event.target.classList.contains("edit")) {
-    let taskNameElement = event.target.parentNode.querySelector(".taskname");
-    let newTaskName = prompt("Enter the new task name:", taskNameElement.textContent.trim());
-    if (newTaskName !== null) {
-      taskNameElement.textContent = newTaskName;
+const submitFunction = (event) => {
+    event.preventDefault();
+    const itemsInput = form.elements['items'];
+
+    if (itemsInput.value.trim() === "") {
+        alert("There is not input. Please enter a value before submitting");
+    } else {
+            //item
+           let newItem = document.createElement('li');
+           let newItemText = document.createElement('div');
+
+           let newItemContent = document.createTextNode(`${itemsInput.value}`);
+           newItemText.appendChild(newItemContent);
+           newItem.appendChild(newItemText);
+           newItem.setAttribute('class', `item`);
+           list.appendChild(newItem);
+            //edit
+            let editButton = document.createElement("BUTTON");
+            let editText = document.createTextNode('Edit');
+            editButton.appendChild(editText);
+            editButton.setAttribute("class", `edit-button` );
+            newItem.appendChild(editButton); 
+            //done
+            let doneButton = document.createElement("BUTTON");
+            let doneText = document.createTextNode('Done');
+            doneButton.appendChild(doneText);
+            doneButton.setAttribute("class", `done-button`);
+            newItem.appendChild(doneButton); 
+            //tick
+            let tickButton = document.createElement("INPUT");
+            tickButton.setAttribute("type", "checkbox")
+            tickButton.setAttribute("class", `tick-button`);
+            newItem.appendChild(tickButton);
+            //delete
+            let deleteButton = document.createElement("BUTTON");
+            let buttonText = document.createTextNode('Delete');
+            deleteButton.appendChild(buttonText);
+            deleteButton.setAttribute("class", `delete-button`);
+            newItem.appendChild(deleteButton); 
     }
-  } else if (event.target.classList.contains("check")) {
-    let taskNameElement = event.target.parentNode.querySelector(".taskname");
-    taskNameElement.classList.toggle("completed");
-  }
-});
+}
+
+form.addEventListener('submit', submitFunction);
+//Conditional to check if you are clicking editButton => event.target.classList.contains("edit-button")
+const editFunction = (event) => {
+    console.log('event', event)
+    event.preventDefault();
+    if(event.target.classList.contains("edit-button")) {
+        const listItem = event.target.parentNode;
+        const listItemText = listItem.firstChild;
+        listItemText.contentEditable = true;
+        listItemText.style.backgroundColor = "#dddbdb";
+    } else if(event.target.classList.contains("done-button")){
+        const listItem = event.target.parentNode;
+        const listItemText = listItem.firstChild;
+        listItemText.contentEditable = false;
+        listItemText.style.backgroundColor = "#ffffff";
+    } else if(event.target.classList.contains("delete-button")) {
+        event.target.parentNode.remove();
+    } else if(event.target.classList.contains("tick-button")){
+        event.target.parentNode.classList.toggle("completed");
+    }
+
+};
+
+listItems.addEventListener('click', editFunction);
